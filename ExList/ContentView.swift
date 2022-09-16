945b8285b9d32dd6fe33cd4611a3edb19a2b568d
jake <palatable7@naver.com>
2022-09-17 00:56:55 +0900

    Remove padding

--- ExList/ContentView.swift
+++ ExList/ContentView.swift
@@ -107,6 +107,7 @@ struct ContentView: View {
           .listRowInsets(EdgeInsets()) // <-
         }
       }
+      .listStyle(PlainListStyle()) // <-
       .navigationTitle("List 예제")
     }
   }
